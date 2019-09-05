5440ad0574dd7feb6d068483c5e65248c660f88e
ayushgw <ayushgoswami@zoho.com>
2019-09-06 00:37:55 +0530

    First GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello GrapgQL');
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+// Resolvers 
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('Server is up!');
+})
