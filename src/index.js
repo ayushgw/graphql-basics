2017c4cbed11355b19d0441e07e723c5b6b6c168
ayushgw <ayushgoswami@zoho.com>
2019-09-07 16:39:48 +0530

    New Queries

--- src/index.js
+++ src/index.js
@@ -3,27 +3,31 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        hello: String!,
-        name: String!,
-        location: String!,
-        bio: String!
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
     }
 `
 
 // Resolvers 
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        id() {
+            return 'abc123'
         },
         name() {
-            return 'Ayush Goswami'
+            return 'Ayush'
         },
-        location() {
-            return 'Jammu'
+        age() {
+            return 27
         },
-        bio() {
-            return 'I like to meditate'
+        employed() {
+            return true
+        },
+        gpa() {
+            return null
         }
     }
 }
