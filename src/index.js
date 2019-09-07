b2b21bd8b46877298ce7e7c7d625a1f72805a588
ayushgw <ayushgoswami@zoho.com>
2019-09-07 16:47:15 +0530

    Scalar Types

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import { GraphQLServer } from 'graphql-yoga'
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        id: ID!
-        name: String!
-        age: Int!
-        employed: Boolean!
-        gpa: Float
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
     }
 `
 
 // Resolvers 
 const resolvers = {
     Query: {
-        id() {
-            return 'abc123'
+        title() {
+            return 'Vibhuti'
         },
-        name() {
-            return 'Ayush'
+        price() {
+            return 2.99
         },
-        age() {
-            return 27
+        releaseYear() {
+            return 2018
         },
-        employed() {
-            return true
+        rating() {
+            return 3.66
         },
-        gpa() {
-            return null
+        inStock() {
+            return false
         }
     }
 }
