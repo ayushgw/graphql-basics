53331bb3689840d679d61cae7cc4dc90bfe78b3f
ayushgw <ayushgoswami@zoho.com>
2019-09-08 18:11:19 +0530

    Relational Data Arrays --Setting posts on each User

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -102,6 +103,13 @@ const resolvers = {
     Post: {
         author(parent, args, ctx, info) {
             return users.find(user => user.id === parent.author)
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter(post => {
+                return post.author === parent.id
+            })
         }
     }
 }
