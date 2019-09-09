7834c949c4d0621bfc1ba3caac26d1e8aa65e8a3
ayushgw <ayushgoswami@zoho.com>
2019-09-09 23:19:12 +0530

    Custom Input Type

--- src/index.js
+++ src/index.js
@@ -13,9 +13,28 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
+        createUser(data: CreateUserInput!): User!
+        createPost(data: CreatePostInput!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
     }
 
     type User {
@@ -85,15 +104,15 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, ctx, info) {
-            const emailTaken = users.some(user => user.email === args.email)
+            const emailTaken = users.some(user => user.email === args.data.email)
 
-            if(emailTaken) {
+            if (emailTaken) {
                 throw new Error('Email Taken!')
             }
 
             const user = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             users.push(user)
@@ -101,15 +120,15 @@ const resolvers = {
             return user
         },
         createPost(parent, args, ctx, info) {
-            const userExists = users.some(user => user.id === args.author)
+            const userExists = users.some(user => user.id === args.data.author)
 
-            if(!userExists) {
+            if (!userExists) {
                 throw new Error('User doesn\'t exist!')
             }
 
             const post = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             posts.push(post)
@@ -117,16 +136,16 @@ const resolvers = {
             return post
         },
         createComment(parent, args, ctx, info) {
-            const userExists = users.some(user => user.id === args.author)
-            const isPostValid = posts.some(post => post.id === args.post && post.published)
+            const userExists = users.some(user => user.id === args.data.author)
+            const isPostValid = posts.some(post => post.id === args.data.post && post.published)
 
-            if(!userExists || !isPostValid) {
-                throw new Error ('Invalid user or post!')
+            if (!userExists || !isPostValid) {
+                throw new Error('Invalid user or post!')
             }
 
             const comment = {
                 id: uuidv4(),
-                ...args
+                ...args.data
             }
 
             comments.push(comment)
@@ -158,7 +177,7 @@ const resolvers = {
         },
         post(parent, args, ctx, info) {
             return posts.find(post => post.id === parent.post)
-        } 
+        }
     }
 }
 
