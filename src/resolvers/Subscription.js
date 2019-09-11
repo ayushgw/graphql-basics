8a789fdcb6a7a53b1671dde4bad9f3535a600854
ayushgw <ayushgoswami@zoho.com>
2019-09-12 01:43:03 +0530

    Subscription --On createPost

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -24,6 +24,11 @@ const Subscription = {
 
             return pubsub.asyncIterator(`comment ${postId}`)
         }
+    },
+    post: {
+        subscribe(parent, args, { pubsub }, info) {
+            return pubsub.asyncIterator('post')
+        }
     }
 }
 
