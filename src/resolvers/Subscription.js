808a3358a7cde14d50083c2a87bea62de4f75916
ayushgw <ayushgoswami@zoho.com>
2019-09-12 01:43:44 +0530

    Removing Count Subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,19 +1,4 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, { pubsub }, info) {
-            let count = 0
-
-            setInterval(() => {
-                count++
-                pubsub.publish('counter', {
-                    count
-                })
-            } ,1000)
-
-            // To keep listening to 'counter' connection
-            return pubsub.asyncIterator('counter')
-        }
-    },
     comment: {
         subscribe(parent, { postId }, { db, pubsub }, info) {
             const post = db.posts.find(post => post.id === postId && post.published)
